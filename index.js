9841062630519fc8d85725ff9efe5876cc6f6909
edsonfervo16mx <edson55_@hotmail.com>
2020-07-22 15:27:57 -0500

    Pruebas con metodos de consulta
    Pruebas de consultas

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
-require("dotenv").config()
-var express = require("express")
-var app = express()
+require("dotenv").config();
+var express = require("express");
+var app = express();
 
-var Model = require('./models/index')
+var Model = require("./models/index");
 
-app.get("/", function (request, response) {
-    console.log(process.env.APP_MESSAGE_SUCCESS)
+app.get("/", function(request, response) {
+  console.log(process.env.APP_MESSAGE_SUCCESS);
 
-    /** Example **/
-    /**/
+  /** Example **/
+  //Insert
+  /*
     Model.Empresa.create({ name: 'rancho aguacate', description: 'rancho', logo: 'aguacate.png', location: 'Comalcalco', rfc: 'agua1234', slogan: 'aguacate', mail: 'aguacate@mail.com', telephone: '93312134545', website: 'aguacate.mx' })
     /**/
 
-    /**/
-    Model.Invitacion.create({ name: 'FI', expiration: '2020-07-22', empresaId: '1' })
+  /*
+    //time + 5hrs
+    Model.Invitacion.create({ name: 'FI', expiration: '2020-07-22 11:45:00', empresaId: '1' })
     /**/
 
-    console.log(process.env.APP_MESSAGE_SUCCESS)
-})
-console.log('Backql en http://localhost:' + process.env.APP_PORT)
-app.listen(process.env.APP_PORT)
+  //Update
+  /*
+    Model.Invitacion.update({ name: 'FIU' }, { where: { id: 3 } })
+    /**/
+
+  //Select All
+  /*
+  var data = [];
+  Model.Invitacion.findAll()
+    .then((data) => {
+      response.json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  /**/
+
+  console.log(data);
+  console.log(process.env.APP_MESSAGE_SUCCESS);
+});
+console.log("Backql en http://localhost:" + process.env.APP_PORT);
+app.listen(process.env.APP_PORT);
